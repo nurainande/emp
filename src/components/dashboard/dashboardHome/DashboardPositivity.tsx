aa737d800deb48bfe5e634e3c6d034b0d830dfd6
nurainande <nurainande@gmail.com>
2025-09-25 00:11:32 -0700

    DashboardOverviewPage version one

--- src/components/dashboard/dashboardHome/DashboardPositivity.tsx
+++ src/components/dashboard/dashboardHome/DashboardPositivity.tsx
@@ -1,26 +1,3 @@
-// import React from 'react'
-
-// import DonutChart from "../../charts/DonutChart";
-
-// const donutData = [
-//   { name: "Bacteria", value: 234, color: "#7B61FF", change: 5 },
-//   { name: "Fungi", value: 24, color: "#A68BFF", change: 35 },
-//   { name: "Virus", value: 2, color: "#F472B6", change: 0.5 },
-//   { name: "Parasite", value: 34, color: "#FB7185", change: 20 },
-//   { name: "Others", value: 5, color: "#E9D5FF", change: 0.2 },
-// ];
-
-// const DashboardPositivity = () => {
-//   return (
-//     <div className='flex'>
-//       {/* <header>Dashboard DashboardPositivity</header> */}
-//       <DonutChart data={donutData} height={260} />
-//       <DonutChart data={donutData} height={260} />
-//     </div>
-//   )
-// }
-
-// export default DashboardPositivity
 
 import React from "react";
 import BubbleCharts from "../../charts/BubbleCharts";
